7a8d42bfeb196c4d36f1e09f1a912d86072bc530
ahmetpalavan <ahmetpalavann@gmail.com>
2023-11-20 13:50:47 +0300

    feat(api): Add code and image routes
    This commit adds two new route files: `route.ts` in the `app/api/code` directory and `route.ts` in the `app/api/image` directory.
    
    In the `code/route.ts` file, a function named `POST` is defined. This function handles POST requests and performs various checks before making an API call to OpenAI's GPT-3.5 Turbo model for code completions.
    
    In the `image/route.ts` file, another function named `POST` is defined. This function also handles POST requests and performs similar checks before making an API call to OpenAI's image generation endpoint.
    
    Both routes handle error cases by returning appropriate responses with status codes.
    
    These changes enable users to generate code snippets and images using the respective routes.

---
+++ app/api/code/route.ts
@@ -0,0 +1,43 @@
+import { auth } from "@clerk/nextjs";
+import { NextResponse } from "next/server";
+import OpenAI from "openai";
+import { ChatCompletionMessageParam } from "openai/resources/index.mjs";
+
+const openai = new OpenAI({
+  apiKey: process.env.OPENAI_API_KEY,
+});
+
+const instructionMessage: ChatCompletionMessageParam = {
+  content: "You are a code generator. You must answer only in markdown code snippets. Use code comments for explanations.",
+  role: "system",
+};
+
+export async function POST(req: Request) {
+  try {
+    const { userId } = auth();
+    const body = await req.json();
+    const { messages } = body;
+
+    if (!userId) {
+      return new NextResponse("Unauthorized", { status: 401 });
+    }
+
+    if (!openai.apiKey) {
+      return new NextResponse("Unauthorized", { status: 401 });
+    }
+
+    if (!messages) {
+      return new NextResponse("Message is required", { status: 400 });
+    }
+
+    const response = await openai.completions.create({
+      model: "gpt-3.5-turbo-instruct",
+      prompt: [instructionMessage, ...messages].map((message) => message.content).join("\n"),
+    });
+
+    return NextResponse.json(response.choices[0].text);
+  } catch (error) {
+    console.log(["error", error]);
+    return new NextResponse("Something went wrong", { status: 500 });
+  }
+}
